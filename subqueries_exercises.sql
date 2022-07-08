5c082ec5bfbf086a24d445051440951daf576e44
Dan Churchill <dan.churchill.satx@gmail.com>
2022-07-08 10:02:46 -0500

    refined answers and formating

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -3,11 +3,15 @@ USE employees;
 
 -- 1. Find all the current employees with the same hire date as employee 101010 using a sub-query.
 SELECT * FROM employees
-WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no = '101010');
-
+WHERE hire_date = (SELECT hire_date FROM employees WHERE emp_no = '101010') AND
+	emp_no IN (SELECT emp_no FROM salaries WHERE to_date > NOW());
+		-- 55 rows returned
 
 -- 2. Find all the titles ever held by all current employees with the first name Aamod.
-SELECT * FROM titles WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+SELECT title FROM titles WHERE emp_no IN (SELECT emp_no FROM employees WHERE first_name = 'Aamod') AND
+	emp_no IN (SELECT emp_no FROM salaries WHERE to_date > NOW())
+GROUP BY title;
+		-- 6 rows returned
 
 
 -- 3. How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
@@ -21,7 +25,8 @@ SELECT * FROM employees WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE t
 
 -- 5. Find all the employees who currently have a higher salary than the companies overall, historical average salary.
 SELECT * FROM employees WHERE emp_no IN(
-	SELECT emp_no FROM salaries WHERE salary > (SELECT AVG(salary) FROM salaries) AND to_date > NOW());
+	SELECT emp_no FROM salaries 
+    WHERE salary > (SELECT AVG(salary) FROM salaries) AND to_date > NOW());
 		-- 154543 employees returned
 
 -- 6. How many current salaries are within 1 standard deviation of the current highest salary? 
@@ -30,19 +35,20 @@ SELECT * FROM employees WHERE emp_no IN(
 	-- get current salaries:  240,124
 SELECT salary FROM salaries WHERE to_date > NOW();
 
-	-- get Mean salary: 72,012.2359
-SELECT AVG(salary) FROM salaries WHERE to_date > NOW();
+	-- get MAX current salary: 158,220
+SELECT MAX(salary) FROM salaries WHERE to_date > NOW();
 
 	-- get Standard deviatioin:  17309.95...
 SELECT STDDEV(salary) FROM salaries WHERE to_date > NOW();
 
-	-- get salaries +/- one std deviation from mean (should be ~68% of 240,124, or ~163,284)
+	-- get salaries between MAX minus  one std deviation from max
 SELECT salary FROM salaries 
 WHERE (to_date > NOW())
-	AND salary BETWEEN (SELECT AVG(salary) FROM salaries WHERE to_date > NOW()) - (SELECT STDDEV(salary) FROM salaries WHERE to_date > NOW())
-	AND (SELECT AVG(salary) FROM salaries WHERE to_date > NOW()) + (SELECT STDDEV(salary) FROM salaries WHERE to_date > NOW())
+	AND salary BETWEEN (SELECT MAX(salary) FROM salaries WHERE to_date > NOW()) - (SELECT STDDEV(salary) FROM salaries WHERE to_date > NOW())
+    AND (SELECT MAX(salary) FROM salaries WHERE to_date > NOW())
 ORDER BY salary;
-				-- returned 161,846 salaries ranging from 54,703 to 89,322
+				-- returned 83 salaries ranging from 140,974 and 158,220
+                -- .0345%
 
 
 -- BONUS
@@ -56,8 +62,6 @@ WHERE E.emp_no IN (SELECT emp_no FROM employees WHERE emp_no IN (SELECT emp_no F
 ;
 
 -- 2. Find the first and last name of the employee with the highest salary.
-SELECT emp_no FROM salaries WHERE to_date > NOW() ORDER BY salary DESC LIMIT 1;
-
 SELECT first_name, last_name FROM employees WHERE emp_no = (
 	SELECT emp_no FROM salaries WHERE to_date > NOW() ORDER BY salary DESC LIMIT 1
 	);
